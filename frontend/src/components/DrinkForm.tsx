8478494f0a231a5b522da8fe1b401b23f41e4e3f
leinson <hannah.leinson@gmail.com>
2024-02-12 14:28:49 +0200

    All coffee fields added to DrinkForm

--- frontend/src/components/DrinkForm.tsx
+++ frontend/src/components/DrinkForm.tsx
@@ -39,14 +39,17 @@ export const DrinkForm: React.FC<DrinkFormProps> = ({
       />
       <label htmlFor="drinkCoffee">Coffee</label>
       <br />
+      <label htmlFor="name">Name</label>
       <input id="name" value={drinkFormData.name} onChange={handleTextChange} />
       <br />
+      <label htmlFor="weight">Weight</label>
       <input
         id="weight"
         value={drinkFormData.weight}
         onChange={handleTextChange}
       />
       <br />
+      <label htmlFor="price">Price</label>
       <input
         id="price"
         value={drinkFormData.price}
@@ -64,12 +67,39 @@ export const DrinkForm: React.FC<DrinkFormProps> = ({
       <br />
       <input
         type="radio"
+        id="mediumRoast"
+        value="Medium roast"
+        checked={drinkFormData.roast === "Medium roast"}
+        onChange={handleRoastChoiceChange}
+      />
+      <label htmlFor="mediumRoast">Medium roast</label>
+      <br />
+      <input
+        type="radio"
         id="darkRoast"
         value="Dark roast"
         checked={drinkFormData.roast === "Dark roast"}
         onChange={handleRoastChoiceChange}
       />
       <label htmlFor="darkRoast">Dark roast</label>
+      <br />
+      <input
+        type="radio"
+        id="espressoRoast"
+        value="Espresso roast"
+        checked={drinkFormData.roast === "Espresso roast"}
+        onChange={handleRoastChoiceChange}
+      />
+      <label htmlFor="espressoRoast">Espresso roast</label>
+      <br />
+      <input
+        type="radio"
+        id="frenchRoast"
+        value="Fremch roast"
+        checked={drinkFormData.roast === "French roast"}
+        onChange={handleRoastChoiceChange}
+      />
+      <label htmlFor="frenchRoast">French roast</label>
       <br />
       <button type="submit">add</button>
     </form>
